148fa6379cb5e124f6bdc0b5e025f4ebc5e8abec
Marty Hope <Marty Hope>
2016-08-15 12:46:26 -0700

    Sample F# Core From: https://docs.microsoft.com/en-us/dotnet/articles/fsharp/getting-started-netcore

---
+++ src/Library/Lib.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+
+module Library
+
+open Newtonsoft.Json
+
+let getJsonNetJson value = 
+    sprintf "I used to be %s but now I'm %s!" value  (JsonConvert.SerializeObject(value))
---
+++ test/TestLibrary/Tests.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+module Test
+
+open Xunit
+open Library
+
+[<Fact>]    
+let ``Library converts "Banana" correctly``() =
+    let expected = """I used to be Banana but now I'm "Banana"!"""
+    let actual =  getJsonNetJson "Banana"
+    Assert.Equal(expected, actual)
