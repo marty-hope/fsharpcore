ebb856166d24e257dabcd61d8fbfdd16d64f4d25
Marty Hope <Marty Hope>
2016-08-15 15:58:34 -0700

    Add 2 list functions Add tests for functions

--- src/Library/Lib.fs
+++ src/Library/Lib.fs
@@ -2,7 +2,15 @@
 
 module Library
 
+open System
 open Newtonsoft.Json
 
 let getJsonNetJson value = 
     sprintf "I used to be %s but now I'm %s!" value  (JsonConvert.SerializeObject(value))
+
+
+let doubleValuesInList list =
+    List.map (fun x -> x * 2) list
+
+let getEvenNumbers list =
+    List.filter (fun x -> x % 2 = 0) list
--- test/TestLibrary/Tests.fs
+++ test/TestLibrary/Tests.fs
@@ -4,9 +4,23 @@ module Test
 
 open Xunit
 open Library
+open System.Collections
 
 [<Fact>]    
 let ``Library converts "Banana" correctly``() =
     let expected = """I used to be Banana but now I'm "Banana"!"""
     let actual =  getJsonNetJson "Banana"
     Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Test double list values``() =
+    let list = [1;2;3;4]
+    let doubledList = doubleValuesInList list
+    Assert.Equal(doubledList.[0], 2)
+
+[<Fact>]
+let ``Test filter even numbers``() =
+    let list = [1;2;3;4;5;6;7;8;9]
+    let evenList = getEvenNumbers list
+    Assert.Equal(evenList.[0], 2)
+    Assert.Equal(evenList.[3], 8)
